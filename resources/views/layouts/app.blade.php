7098ae48651253da3fc903f931e6793dff4f91a2
Everton Mendonca <evertonmj@gmail.com>
2016-04-26 15:04:50 -0300

    Menus and home page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -38,9 +38,30 @@
 
     <body>
         <div class="container">
-            <nav class="navbar navbar-default">
-                <!-- Navbar Contents -->
-            </nav>
+              <nav class="navbar navbar-default">
+                <div class="container-fluid">
+                  <!-- Brand and toggle get grouped for better mobile display -->
+                  <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                      <span class="sr-only"></span>
+                      <span class="icon-bar"></span>
+                      <span class="icon-bar"></span>
+                      <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="/">Momento Esportivo</a>
+                  </div>
+
+                  <!-- Collect the nav links, forms, and other content for toggling -->
+                  <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                    <ul class="nav navbar-nav">
+                      <li><a href="/event">Eventos</a></li>
+                      <li><a href="#">Momentos</a></li>
+                      <li><a href="#">Times</a></li>
+                      <li><a href="#">Usuário</a></li>
+                    </ul>
+                  </div><!-- /.navbar-collapse -->
+                </div><!-- /.container-fluid -->
+                </nav>
         </div>
 
         @yield('content')
